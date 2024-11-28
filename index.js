bbecbefc7d19b74be7eaa06356bcbf6ab1d3092c
sangavi06 <146512549+sangavi06@users.noreply.github.com>
2024-11-28 20:16:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,10 +39,7 @@ app.use(express.json());
 // CORS configuration
 app.use(
   cors({
-    origin: [
-      process.env.CLIENT_URL,
-      "https://airbnb-frontend.netlify.app",
-    ],
+    origin: ["https://airbnb-frontend.netlify.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
