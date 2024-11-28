4307958ff551007e15509366077ac4efc0e2f0a1
sangavi06 <146512549+sangavi06@users.noreply.github.com>
2024-11-28 20:27:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const cookieSession = require("cookie-session");
 const cookieParser = require("cookie-parser");
 const cloudinary = require("cloudinary").v2;
 
-// Connect to database
+// connect with database
 connectWithDB();
 
-// Configure Cloudinary
+// cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -18,7 +18,7 @@ cloudinary.config({
 
 const app = express();
 
-// Handle cookies
+// For handling cookies
 app.use(cookieParser());
 
 // Initialize cookie-session middleware
@@ -27,30 +27,31 @@ app.use(
     name: "session",
     maxAge: process.env.COOKIE_TIME * 24 * 60 * 60 * 1000,
     keys: [process.env.SESSION_SECRET],
-    secure: true,
-    sameSite: "none",
-    httpOnly: true,
+    secure: true, // Only send over HTTPS
+    sameSite: "none", // Allow cross-origin requests
+    httpOnly: true, // Makes the cookie accessible only on the server-side
   })
 );
 
-// Parse JSON requests
+// middleware to handle json
+app.use(express.json());
+
+// CORS
 app.use(
   cors({
-    origin: ["*"],
+    origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-// Use express router
+// use express router
 app.use("/", require("./routes"));
 
 app.listen(process.env.PORT || 8000, (err) => {
   if (err) {
-    console.log("Error connecting to server: ", err);
+    console.log("Error in connecting to server: ", err);
   }
-  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server is running on port no. ${process.env.PORT}`);
 });
 
 module.exports = app;
